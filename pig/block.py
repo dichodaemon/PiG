b9d78b3d5f5fc2e706c154be8ff9fd1c8f3fae45
Dizan Vasquez <dichodaemon@gmail.com>
2011-03-16 13:48:40 -0600

    Add started attribute to block

--- pig/block.py
+++ pig/block.py
@@ -6,9 +6,10 @@ import image_manager
 class Block( object ):
   def __init__( self, width, height, color = ( 0, 0, 0, 0 ) ):
     self.frame = image_manager.block( width, height, color )
+    self.started = False
 
   def start( self, time ):
-    pass
+    self.started = True
 
   def finished( self ):
     return True
